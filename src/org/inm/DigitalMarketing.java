ef8857de4fbfa99fd97f5ea31279a7aa85dfea36
DivyaBharathiSangee <bharathisangee100@gmail.com>
2023-10-09 19:50:44 +0530

    Inmakes Database added

---
+++ src/org/inm/DigitalMarketing.java
@@ -0,0 +1,30 @@
+package org.inm;
+
+public class DigitalMarketing {
+	private void blogging() {
+		System.out.println("Blogging");
+
+	}
+	private void searchEngine() {
+		System.out.println("SearchEngine");
+
+	}
+	private void contentMarketing() {
+		System.out.println("ContentMarketing");
+
+	}
+	private void socialMedia() {
+		System.out.println("SocialMedia");
+
+	}
+
+	public static void main(String[] args) {
+		DigitalMarketing d= new DigitalMarketing();
+		d.blogging();
+		d.searchEngine();
+		d.contentMarketing();
+		d.socialMedia();
+
+	}
+
+}
